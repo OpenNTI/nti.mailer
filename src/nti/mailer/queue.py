76e13cc85a3a0d62b77a3f20a20a4b5514f90696
cutz <christopher.m.utz@gmail.com>
2019-11-05 17:18:03 -0600

    remove unicode literals

--- src/nti/mailer/queue.py
+++ src/nti/mailer/queue.py
@@ -7,7 +7,7 @@ for the ``qp`` command line, using Amazon SES.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
